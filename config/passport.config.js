34bdb35bdb633b8a99d2a6db6f86a5cc3db54794
Irma Blanco <irblancofdz@gmail.com>
2020-09-14 02:17:38 +0200

    feat: test google login

--- config/passport.config.js
+++ config/passport.config.js
@@ -8,7 +8,9 @@ const google = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    // callbackURL: "/auth/google/callback",
+    callbackURL: "https://iron-appyhour.herokuapp.com/auth/google/callback",
+    // callbackURL: "http://localhost:3000/auth/google/callback",
   },
   (accessToken, refreshToken, profile, next) => {
     User.findOne({ "social.googleID": profile.id })
